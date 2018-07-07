cbadcc8c2bb94dc0ad1ffc77dc951e7c77843e7f
UselessPickles <jlau@uselesspickles.com>
2018-07-07 02:01:26 -0400

    Improve dtslint test

--- type_tests/Stopwatch.dtslint.ts
+++ type_tests/Stopwatch.dtslint.ts
@@ -1,14 +1,13 @@
 import { Stopwatch } from "../dist/types";
 
-// $ExpectType Stopwatch
-const stopwatch = new Stopwatch();
-
 // Verify that Slices are readonly
-// $ExpectType Slice
-const slice = stopwatch.getPendingSlice();
+declare const slice: Stopwatch.Slice;
 // $ExpectError
 slice.startTime = 0;
+slice.startTime; // will error if Slice definition changes
 // $ExpectError
 slice.endTime = 0;
+slice.endTime; // will error if Slice definition changes
 // $ExpectError
 slice.duration = 0;
+slice.duration; // will error if Slice definition changes
