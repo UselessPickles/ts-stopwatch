07e9a41347c1d772845d59168a831f7887abe8b7
UselessPickles <jlau@uselesspickles.com>
2018-07-07 01:29:07 -0400

    Fix copy/paste error in comment

--- type_tests/index.d.ts
+++ type_tests/index.d.ts
@@ -1,3 +1,3 @@
 // Dummy index.d.ts file for running dtslint.
-// We're not actually linting a .d.ts file; only using dtslint to verify compile time types of ts-enum-util.
+// We're not actually linting a .d.ts file; only using dtslint to verify compile time types of ts-stopwatch.
 // TypeScript Version: 2.0
